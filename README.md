e825d7ebec1dac5bfb0cdec14bc06a286a82752a
Natalia Rubio <nataliav.rubio@gmail.com>
2025-03-07 20:34:55 -0300

    agrego readme e instalo CORS

---
+++ README.md
@@ -0,0 +1,15 @@
+# LISTA DE TAREAS BACKEND
+
+> *Proyecto propio*
+
+Instalo CORS para que funcione la comunicación entre el front y el back.
+
+Agrego readme.
+
+Se crean los siguientes endpoints:
+- POST (se crea una nueva tarea)
+- GET (se obtienen todas las tareas o una en específico por ID)
+- DELETE (se elimina una tarea en específico por ID)
+- PUT (se modifica una tarea)
+
+<ins>Herramientas utilizadas:</ins> EXPRESS, MONGO DB
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "./database.js";
 import taskRouter from "./routes/task.router.js";
+import cors from 'cors';
 
 const app = express();
 const PUERTO = 8080;
@@ -8,6 +9,7 @@ const PUERTO = 8080;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("./src/public"));
+app.use(cors({origin: ['http://localhost:5500', 'http://127.0.0.1:5500']}));
 
 app.use("/api/tasks", taskRouter);
 
